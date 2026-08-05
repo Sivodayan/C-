fc0b29df71086ee31edfc72aaaa06af8a37857d6
Sivodayan <sivodayan17@gmail.com>
2026-08-05 19:43:43 +0530

    class

--- p1.cpp
+++ p1.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
-
-main()
+class Book{
+    public:
+    string title;
+    string author;
+    int year;
+};
+int main()
 {
-    cout<<"Hello World";
-    return 0;
+    Book harryPotter;
+    harryPotter.title = "Harry Potter and the Sorcerer's Stone";
+    harryPotter.author = "J.K. Rowling";
+    harryPotter.year = 1997;
+
+    Book theHobbit;
+    theHobbit.title = "The Hobbit";
+    theHobbit.author = "J.R.R. Tolkien";
+    theHobbit.year = 1937;
 }
